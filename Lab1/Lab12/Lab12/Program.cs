a73b19439c13bbff8780049ecb14212c4e39b44c
Argetim Ramadani <argdev@outlook.com>
2020-01-28 16:59:15 +0100

    Refactoring and finished Lab1.2

--- Lab1/Lab12/Lab12/Program.cs
+++ Lab1/Lab12/Lab12/Program.cs
@@ -22,37 +22,43 @@ namespace Lab12
 			do
 			{
 				isValid = true;
-				switch (Convert.ToInt32(Console.ReadLine()))
+				int choice = Convert.ToInt32(Console.ReadLine());
+				isValid = choice <= 2;
+
+				if (isValid)
 				{
-					case 1:
-						Console.WriteLine("Generating {0} random numbers...", N);
-						for (int i = 0; i < N; i++)
-						{
-							numbers[i] = random.Next(1, 1000);
-							sum += (double)numbers[i];
-						}
-						break;
-						
-					case 2:
-						Console.WriteLine("Enter {0} numbers:", N);
-						for (int i = 0; i < N; i++)
-						{
-							numbers[i] = Convert.ToInt32(Console.ReadLine());
-							sum += (double)numbers[i];
-						}
-						break;
-						
-					default:
-						Console.WriteLine("Invalid Choice");
-						isValid = false;
-						break;
+					GenerateOrReadNumbers(choice, ref numbers, ref sum);
+					break;
 				}
+				else
+					Console.WriteLine("Invalid choice!\n1 - Generate random numbers\n2 - Enter numbers manually");
 			}
 			while (!isValid);
 
 			Console.WriteLine("The average of your numbers is: {0}", sum / numbers.Length);
 			Console.WriteLine("The max number of your numbers is: {0}", numbers.Max());
 			Console.WriteLine("The min number of your numbers is: {0}", numbers.Min());
+		}
+
+		/// <summary>
+		/// Generating random numbers or reading them from Console.ReadLine() depending on the choice
+		/// 1 - Generate random numbers
+		/// 2 - Enter numbers manually
+		/// </summary>
+		/// <param name="choice"></param>
+		/// <param name="numbers"></param>
+		/// <param name="sum"></param>
+		private static void GenerateOrReadNumbers(int choice, ref int[] numbers, ref double sum)
+		{
+			Random random = new Random();
+			string message = (choice == 1) ? "Generating {0} random numbers..." : "Enter {0} numbers:";
+			Console.WriteLine(message, numbers.Length);
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				numbers[i] = (choice == 1) ? random.Next(1, 1000) : Convert.ToInt32(Console.ReadLine());
+				sum += (double)numbers[i];
+			}
 		}
 	}
 }
