6e30a3cb43f6ed0056e928e89671e20916b4c4d1
Argetim Ramadani <argdev@outlook.com>
2020-01-29 03:36:18 +0100

    Implemented labs 13, 14 and 15 but not finished yet...

---
+++ Lab1/Lab15/Lab15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab15
+{
+	class Program
+	{
+		enum PasswordStrength 
+		{
+			easy,
+			normal,
+			hard
+		}
+
+		 
+		static void Main(string[] args)
+		{
+			String[] passwords = Console.ReadLine().Split(' ');
+
+			Console.WriteLine("You entered these passwords:");
+			foreach (string psw in passwords) Console.Write("- {0}", psw);
+
+			var easyPaswObj = new
+			{
+				minLength = 0,
+				maxLength = 6
+			};
+
+			Console.WriteLine("Generating passwords...");
+			string easyPasw = generatePassword(PasswordStrength.easy);
+			string normalPasw = generatePassword(PasswordStrength.normal);
+			string hardyPasw = generatePassword(PasswordStrength.hard);
+		}
+
+		static string generatePassword(PasswordStrength passwordStrength)
+		{
+			switch (passwordStrength)
+			{
+				case PasswordStrength.easy:
+					return getPassword(1, 6, true, false, false, false);
+			}
+			return null;
+		}
+
+		static string getPassword(int minLength, int maxLength, bool lowercase, bool uppercase, bool digits, bool specialchars)
+		{
+
+		}
+	}
+}
